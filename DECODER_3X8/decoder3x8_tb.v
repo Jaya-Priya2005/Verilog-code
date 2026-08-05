055cf62c3e06a7b9af1e6ca1436d7c09569906cb
Jaya-Priya2005 <jayapriya2415@gmail.com>
2026-08-05 23:39:19 +0530

    decoder3x8_tb.v

---
+++ DECODER_3X8/decoder3x8_tb.v
@@ -0,0 +1,21 @@
+module decoder_tb;
+  reg [0:2] d;
+  wire [0:7] y;
+    
+decoder_dataflow dut(d,y);
+
+initial begin
+ $monitor(d,y);
+   d = 3'b000;#10;
+   d = 3'b001;#10;
+   d = 3'b010;#10;
+   d = 3'b011;#10;
+   d = 3'b100;#10;
+   d = 3'b101;#10;
+   d = 3'b110;#10;
+   d = 3'b111;#10;
+ #10$finish;
+ 
+end
+   
+endmodule
